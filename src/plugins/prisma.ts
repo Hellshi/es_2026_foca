92a76f8aab2c690a3ceb133b2fc19caef1c688c0
Hellshi <hcslimaa@gmail.com>
2026-06-06 18:49:35 -0300

    feat: adds linter and tests setup

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,10 +1,12 @@
-import fp from 'fastify-plugin'
-import { FastifyPluginAsync } from 'fastify'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../generated/client'
+import fp from 'fastify-plugin';
+import { FastifyPluginAsync } from 'fastify';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/client';
 
 declare module 'fastify' {
-  interface FastifyInstance { prisma: PrismaClient }
+  interface FastifyInstance {
+    prisma: PrismaClient;
+  }
 }
 
 const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
@@ -12,12 +14,12 @@ const prismaPlugin: FastifyPluginAsync = fp(async (server, options) => {
     adapter: new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     }),
-  })
-  await prisma.$connect()
-  server.decorate('prisma', prisma)
+  });
+  await prisma.$connect();
+  server.decorate('prisma', prisma);
   server.addHook('onClose', async (server) => {
-    await server.prisma.$disconnect()
-  })
-})
+    await server.prisma.$disconnect();
+  });
+});
 
-export default prismaPlugin
+export default prismaPlugin;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,9 @@
-import { type FastifyPluginAsync } from 'fastify'
+import { type FastifyPluginAsync } from 'fastify';
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/user', async function (request, reply) {
-    return 'this is an example'
-  })
-}
+    return 'this is an example';
+  });
+};
 
-export default example
+export default example;
