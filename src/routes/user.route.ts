83ae41ebd5a9f93b3ba5ee49a10fbe2102ab3897
Hellshi <hcslimaa@gmail.com>
2026-06-06 14:46:02 -0300

    feat: configurates prisma and ran db

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { type FastifyPluginAsync } from 'fastify'
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
+  fastify.get('/user', async function (request, reply) {
     return 'this is an example'
   })
 }
